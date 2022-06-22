0344cba047d635f2f2fa723a1158ab7a04b29717
Krisna Gusti <k_gusti@hotmail.com>
2022-06-22 19:15:54 +0930

    Updated insertion_sort algorithm

--- insertion_sort.cc
+++ insertion_sort.cc
@@ -1,23 +1,43 @@
 /**
  * @file insertion_sort.cc
- * @brief 
- * 
+ * @brief Insertion sort function that sorts an array of integers
+ * Time complexity: O(n)
  */
 
 #include <iostream>
 
 using namespace std;
 
-
+/**
+ * @brief Sorts an integer array using insertion sort.
+ * 
+ * @param seq_numbers array of integers to sort
+ * @param length length of int array
+ */
 void insertion_sort(int* seq_numbers, int length)
 {
+    int number;
+    int i;
 
+    for (int j = 1; j != length; ++j) {
+        
+        number = seq_numbers[j];
+        i = j - 1;
+
+        // Find where to insert the number
+        while (i >= 0 && seq_numbers[i] > number) {
+            seq_numbers[i + 1] = seq_numbers[i];
+            --i;
+        }
+        
+        seq_numbers[i + 1] = number;
+    }
 }
 
+/* Print an array to console */
 void print_array(int* array, int length) {
-    for (int i = 0; i != length; ++i) {
+    for (int i = 0; i != length; ++i)
         cout << array[i] << " ";
-    }
 }
 
 /** Driver for testing */
